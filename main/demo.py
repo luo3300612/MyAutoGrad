e91e2e7395b6dfbb8d051b90a5e182d71eb21c6f
luo3300612 <591486669@qq.com>
2018-10-26 08:29:10 +0800

    add mat mul

--- main/demo.py
+++ main/demo.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+
 class Node:
     def __init__(self, num=None, show=True):
         self.value = num
@@ -70,27 +71,66 @@ class Mat:
     def __init__(self, elements=None):
         self.m = 0
         self.n = 0
+        self.fathers = []
+        self.chirdren = []
+        self.oper = None
         if elements is not None:
             self.m = len(elements)
             self.n = len(elements[0])
         self.mat = []
-        for i in range(self.n):
+        for i in range(self.m):
             row = []
-            for j in range(self.m):
+            for j in range(self.n):
                 row.append(Node(elements[i][j], show=False))
             self.mat.append(row)
 
-    def __add__(self, other):
+
+    def cal_base(self, other, oper):
         assert self.m == other.m and self.n == other.n
         ret = Mat()
         ret.m = self.m
         ret.n = self.n
         ret.mat = []
+
+        ret.fathers = [self, other]
+        self.chirdren.append(ret)
+        other.fathers.append(ret)
+        self.oper = oper
+
         for i in range(self.n):
             row = []
             for j in range(self.m):
-                new_node = self.mat[i][j] + other.mat[i][j]
+                new_node = eval("self.mat[i][j]" +oper +"other.mat[i][j]")
                 new_node.show = False
+                row.append(new_node)
+            ret.mat.append(row)
+        return ret
+
+    def __add__(self, other):
+        return self.cal_base(other, '+')
+
+    def __sub__(self, other):
+        return self.cal_base(other, '-')
+
+    def __mul__(self, other):
+        assert self.n == other.m
+        ret = Mat()
+        ret.m = self.m
+        ret.n = other.n
+        ret.mat = []
+
+        ret.fathers = [self, other]
+        self.chirdren.append(ret)
+        other.fathers.append(ret)
+        self.oper = '*'
+
+        for i in range(ret.m):
+            row = []
+            for j in range(ret.n):
+                new_node = Node(0, show=False)
+                for k in range(self.n):
+                    new_node = new_node + self.mat[i][k] * other.mat[k][j]
+                    new_node.show = False
                 row.append(new_node)
             ret.mat.append(row)
         return ret
@@ -102,6 +142,7 @@ class Mat:
 
 
 if __name__ == "__main__":
+    # test for long term gradient
     # A = Node(2)
     # B = Node(3)
     # C = A * B
@@ -117,10 +158,37 @@ if __name__ == "__main__":
     # print(G.grad(D))
     # print(G.grad(E))
     # print(G.grad(F))
+
+    # test for identical fathers
     # A = Node(2)
     # B = A * A
     # C = A * B
     # print(B.grad(A))
-    mat1 = Mat([[1,2,3],[4,5,6],[7,8,9]])
-    mat2 = Mat([[3, 1,9], [4, 5, 6], [7, 8, 9]])
-    print(mat1+mat2)
+
+    # test for matrix plus
+    # mat1 = Mat([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+    # mat2 = Mat([[3, 1, 9], [4, 5, 6], [7, 8, 9]])
+    # print(mat1 + mat2)
+
+    # test for in-position
+    # A = Node(2)
+    # B = Node(3)
+    # C = Node(4)
+    #
+    # A = A * B
+    # A = A * C
+    # print(A.grad(B))
+    # print(A.grad(C))
+
+    # test for matrix mul
+    mat1 = Mat([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+    mat2 = Mat([[3, 1,1], [1,4, 5], [1,7, 8]])
+    mat3 = mat1 * mat2
+    mat4 = mat1 + mat2
+    mat5 = mat1 - mat2
+    print(mat1)
+    print(mat2)
+    print(mat3)
+    print(mat4)
+    print(mat5)
+
