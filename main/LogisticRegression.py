83e4b0ee7426b2672eded04a65d5a54af905290c
luo3300612 <591486669@qq.com>
2018-11-01 01:34:21 +0800

    add example LogisticRegression

--- main/LogisticRegression.py
+++ main/LogisticRegression.py
@@ -1,20 +1,44 @@
-from data_structure import Mat
+from data_structure import Mat, Node
 import numpy as np
 import matplotlib.pyplot as plt
+from op import op
 
-x = np.random.uniform(0, 5, (100, ))
-y = np.random.uniform(0, 5, (100, ))
+x = np.random.uniform(0, 5, (100,))
+y = np.random.uniform(0, 5, (100,))
 label = np.zeros((100,))
 
-index = 0
-for _x,_y in zip(x,y):
-    if _x - _y  > 0:
-        label[index] = 1
+for i, cord in enumerate(zip(x, y)):
+    _x, _y = cord
+    if _x - _y > 0:
+        label[i] = 1
     else:
-        label[index] = 0
-    index += 1
+        label[i] = 0
+    if np.random.uniform(0, 1) > 0.95:
+        label[i] = 1 - label[i]
+
+plt.scatter(x[label == 1], y[label == 1])
+plt.scatter(x[label == 0], y[label == 0])
+
+alpha = 0.01
+max_iteration = 100
+
+X = np.vstack((np.ones(100, ), x))
+X = np.vstack((X, y))
+mat_X = Mat(X).T()
+mat_label = Mat([label]).T()
+weight = Mat.zeros(3, 1)
 
 
-plt.scatter(x[label==1],y[label==1])
-plt.scatter(x[label==0],y[label==0])
+for epoch in range(max_iteration):
+    weight.zero_grad()
+    loss = - mat_label.T() * op.log(1 / (1 + op.exp(-mat_X * weight))) - (1 - mat_label).T() * op.log(1 - 1 / (1 + op.exp(-mat_X * weight)))
+
+    weight = weight - alpha * loss.grad(weight)
+
+    if epoch % 10 == 0:
+        print(f"epoch:{epoch},loss:{loss}")
+
+x = np.linspace(0, 5, 50)
+y = list(map(lambda x: (-weight[0][0].value - weight[1][0].value * x) / weight[2][0].value, x))
+plt.plot(x, y)
 plt.show()
