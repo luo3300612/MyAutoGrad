5f49dac773ad8056a61e6d648f95ffc935d46d92
luo3300612 <591486669@qq.com>
2018-11-03 08:05:58 +0800

    add test

--- main/test.py
+++ main/test.py
@@ -4,4 +4,4 @@ from op import op
 if __name__ == "__main__":
     # test for scalar grad Mat
     matA = Mat([[1,2], [2,3]])
-    print(op.norm_suqare(matA).grad(matA))
+    print(op.norm_square(matA).grad(matA))
