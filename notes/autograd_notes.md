45cae0734aba398296ab2449747c6805831a32da
luo3300612 <591486669@qq.com>
2018-11-12 02:49:22 +0800

    update

--- notes/autograd_notes.md
+++ notes/autograd_notes.md
@@ -1,1 +1,75 @@
 # Note
+https://github.com/HIPS/autograd/blob/master/docs/tutorial.md
+https://docs.scipy.org/doc/numpy-1.15.1/user/c-info.how-to-extend.html
+
+## 结构
+### util.py
+* subvals(x:tuple, ivs:list[(i,v)]) -> let x[i] = v 
+* subval(x:tuple, i:int, v:num) -> let x[i] = v
+### wrap_util.py
+**unary_to_nary** ???
+
+**wraps(fun,...)**
+```angular2html
+wraps(f)
+def g(x)
+    return x + 2
+let g.__name__ = f.__name__
+let g.__doc__ = f.__doc__
+```
+* wrap_nary_f
+### defferential_operators.py
+* grad(fun, x) 返回
+
+### core.py
+
+### tracer.py
+**Node类**
+
+Node类是一个抽象类，其中有三个方法
+* __init__ 初始化方法
+* initialize_root
+* new_root 类方法(被classmethod装饰)
+
+前两个方法都只有一行 assert False，表明Node类是一个抽象类，无法创造实例。
+
+new_root方法通过调用__new__创建实例，再调用initialize_root
+
+
+## 装饰器
+### wraps
+wraps(fun, namestr="{fun}", docstr="{doc}", **kwargs)
+
+
+
+## 笔记
+### warning 的写法
+```angular2html
+import warnings
+warning_msg = """This is a warning message"""
+
+def fun_warning():
+    warnings.warn(warning_msg)
+
+```
+
+### grad
+grad包裹一个实值函数之后，与实值函数接受相同的参数，但返回的是实值函数的导数
+
+### 抽象类
+子类通过__new__和initialize_root方法创建实例
+```angular2html
+class Node(object):
+    __slots__ = []
+    def __init__(self, value, fun, args, kwargs, parent_argnums, parents):
+        assert False
+
+    def initialize_root(self, *args, **kwargs):
+        assert False
+
+    @classmethod
+    def new_root(cls, *args, **kwargs):
+        root = cls.__new__(cls)
+        root.initialize_root(*args, **kwargs)
+        return root
+```
--- notes/test.py
+++ notes/test.py
@@ -2,15 +2,47 @@ import autograd.numpy as np
 from autograd import grad
 
 
-def taylor_sine(x):
-    ans = currterm = x
-    i = 0
+# test1
+# def taylor_sine(x):
+#     ans = currterm = x
+#     i = 0
+#
+#     while np.abs(currterm) > 0.001:
+#         currterm = - currterm * x ** 2 / ((2 * i + 3) * (2 * i + 2))
+#         ans = ans + currterm
+#         i += 1
+#     return ans
+#
+# grad_sine = grad(taylor_sine)
+# print("gradient of sin(pi) is", grad_sine(np.pi))
 
-    while np.abs(currterm) > 0.001:
-        currterm = - currterm * x ** 2 / ((2 * i + 3) * (2 * i + 2))
-        ans = ans + currterm
-        i += 1
-    return ans
+# test2
+def sigmoid(x):
+    return 0.5 * (np.tanh(x / 2) + 1)
 
-grad_sine = grad(taylor_sine)
-print("gradient of sin(pi) is", grad_sine(np.pi))
+
+def logistic_predictions(weights, inputs):
+    return sigmoid(np.dot(inputs, weights))
+
+
+def trainning_loss(weights):
+    preds = logistic_predictions(weights, inputs)
+    label_probabilities = preds * targets + (1 - preds) * (1 - targets)
+    return -np.sum(np.log(label_probabilities))
+
+
+inputs = np.array([[0.52, 1.12, 0.77],
+                   [0.88, -1.08, 0.15],
+                   [0.52, 0.06, -1.30],
+                   [0.74, -2.49, 1.39]])
+targets = np.array([True, True, False, True])
+
+trainning_graient_fun = grad(trainning_loss)
+
+weights = np.array([0.0, 0.0, 0.0])
+
+print("Initial loss:", trainning_loss(weights))
+
+for i in range(100):
+    weights -= trainning_graient_fun(weights) * 0.01
+    print("epoch loss:", trainning_loss(weights))
